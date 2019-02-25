bf4dda32c02c929c84af446d14990a0c288dc2b2
Jim <easonjim@163.com>
2019-02-25 10:45:37 +0800

    add

--- ss-install.sh
+++ ss-install.sh
@@ -10,10 +10,10 @@ export PATH
 cur_dir=`pwd`
 
 libsodium_file="libsodium-1.0.16"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz"
+libsodium_url="https://raw.githubusercontent.com/easonjim/shadowsocks-shell/master/lib_file/libsodium-1.0.16.tar.gz"
 
 mbedtls_file="mbedtls-2.13.0"
-mbedtls_url="https://tls.mbed.org/download/mbedtls-2.13.0-gpl.tgz"
+mbedtls_url="https://raw.githubusercontent.com/easonjim/shadowsocks-shell/master/lib_file/mbedtls-2.13.0-gpl.tgz"
 
 # Stream Ciphers
 ciphers=(
